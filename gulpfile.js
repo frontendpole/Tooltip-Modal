f5c3624260ce9b4db5b8c50f54adec489310743d
Aleksandra <aleksandra.karp1@gmail.com>
2020-02-05 22:49:41 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const scssSrc = './src/scss/*.scss';
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', () => {
+    return gulp.src(scssSrc)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('sass:watch', () => {
+    gulp.watch(scssSrc, gulp.parallel('sass'));
+});
+
+gulp.task('copy-js', () => {
+    return gulp.src('./src/js/*.js')
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy-js:watch', () => {
+    gulp.watch('./src/js/*.js', gulp.parallel('copy-js'));
+});
+
+gulp.task('watch', gulp.parallel('sass:watch', 'copy-js:watch'));
+
+gulp.task('copy-html', () => {
+    return gulp.src('./src/*.html')
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy-html:watch', () => {
+    gulp.watch('./src/*.html', gulp.parallel('copy-html'));
+});
